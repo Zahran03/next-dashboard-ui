4c29958da3538df40a92187f357eaba5fb9f9b61
Zahran03 <zalfarizi502@gmail.com>
2025-01-11 07:57:28 +0700

    feat : create navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
 
 export default function RootLayout({
   children,
@@ -21,7 +22,10 @@ export default function RootLayout({
         <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:*:w-[86%] bg-[#F7F8FA]"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,48 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/* search bar */}
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+        <Image src="/search.png" alt="searchLogo" width={14} height={14} />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] p-2 bg-transparent outline-none"
+        />
+      </div>
+      {/* icons and user */}
+      <div className="flex items-center gap-6 justify-end w-full">
+        {/* message */}
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer ">
+          <Image src="/message.png" alt="messageIcon" width={20} height={20} />
+        </div>
+        {/* announcement */}
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
+          <Image
+            src="/announcement.png"
+            alt="annountcemenetIcon"
+            width={20}
+            height={20}
+          />
+        </div>
+        {/* user & role */}
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+        <Image
+          src="/avatar.png"
+          alt="avatarIcon"
+          width={36}
+          height={36}
+          className="rounded-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
