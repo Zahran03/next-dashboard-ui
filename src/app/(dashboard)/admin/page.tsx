bd3962b0a753d804c9ede5b5d1775745a92b1cbb
Zahran03 <zalfarizi502@gmail.com>
2025-01-12 09:58:11 +0700

    feat : create students count chart in admin pgae

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/app/components/CountChart";
 import UserCard from "@/app/components/UserCard";
 import React from "react";
 
@@ -6,13 +7,27 @@ const page = () => {
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* left section */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* user cards */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* middle chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count charts */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* atttendance chart */}
+          <div className="w-full lg:h-2/3 h-[450px]">r</div>
+        </div>
+        {/* bottom chart */}
+        <div className="w-full h-[500px]"></div>
       </div>
+      {/* right section */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8"></div>
     </div>
   );
 };
